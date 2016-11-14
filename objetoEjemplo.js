9b85c3a99f631a24f31dcab31ad7583974f4aa2a
jhoemsbry <jhoemsbry@gmail.com>
2016-11-14 13:50:01 -0400

    iniciando repo

---
+++ objetoEjemplo.js
@@ -0,0 +1,4 @@
+var pelicula={titulo:"moster",genero:"caricatura",director:"toys",duracion:60}
+console.log("perlicula: "+pelicula.titulo);
+console.log("perlicula: "+pelicula.genero);
+console.log("perlicula: "+pelicula.duracion);
---
+++ operadorigualdad.js
@@ -0,0 +1,2 @@
+
+ +console.log("la comparacion entre 4 === '4' es :" + (4 === "4") ) 
